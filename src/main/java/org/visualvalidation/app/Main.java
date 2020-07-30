41bdeed7c8cefb3668306b08f6e6f3ed2a52c674
rubeninkafarma <ruben.soria@inkafarmadigital.pe>
2020-07-30 16:35:52 -0500

    pase a prod

--- src/main/java/org/visualvalidation/app/Main.java
+++ src/main/java/org/visualvalidation/app/Main.java
@@ -1,5 +1,15 @@
 package org.visualvalidation.app;
 
 public class Main {
+	public static void main(String[] args) {
+		// Hola a todos
+		
+		enviarAProd();
+
+	}
+	
+	public static void enviarAProd() {
+		System.out.println("Esto si debe ir a prod");
+	}
 
 }
